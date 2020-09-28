4910c5d86f8fc727e110a59fd34f84bea359774d
Ranger-boop <70635684+Ranger-boop@users.noreply.github.com>
2020-09-28 14:19:22 +0800

    Add files via upload

---
+++ myfirst.cpp
@@ -0,0 +1,13 @@
+// myfirst.cpp--display a message
+
+#include <iostream>								// a preprocesor directive
+
+int main()										// function header
+{												// start of function body
+	using namespace std;						// make definitions visible
+	cout << "come up and C++ me some time.\n";	// message
+	cout << endl;								// start a new line
+	cout << "You won't regret it!" << endl;
+	cin.get();
+	return 0;									// terminate main()
+}												// end of function body
